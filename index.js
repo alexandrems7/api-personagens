73a2c405c21fd312c5954fef121e1478078c3662
alexandrems7 <alexandremartins770@gmail.com>
2022-08-08 19:19:31 -0300

    commit 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 5000;
+const port = 3000;
 const app = express();
 const cors = require('cors');
 const routes = require('./src/routes/personagem.route');
